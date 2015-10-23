78ab67b37cf4705dd45e2cfdb107e198ea7dec27
Pablo Dias <pablodias89@hotmail.com>
2015-10-23 11:58:15 -0200

    Tabela Modelo

--- controller/AlunosController.php
+++ controller/AlunosController.php
@@ -8,30 +8,29 @@ class AlunosController {
         $bancoAlunos = new AlunosModel();
         $bancoAlunos->todasColunas();
         $result = $bancoAlunos->executar();
-        $resultado = $this->tabelarResultado($result);      
+        $resultado = $this->tabelarResultado($result);
 
         return $resultado;
     }
 
     public function tabelarResultado($resultado) {
-        
+
         $tabela = '<table class="table">';
 
         if ($resultado->num_rows > 0) {
             while ($row = $resultado->fetch_assoc()) {
-                
+
                 foreach ($row as $key => $value) {
                     $tabela .= '<tr>';
                     $tabela .= "<td>$key</td><td>$value</td>";
                     $tabela .= '</tr>';
                 }
-                
             }
-        $tabela .= '</table>';
+            $tabela .= '</table>';
         } else {
             $tabela = "0 results";
         }
-        
+
         return $tabela;
     }
 
--- model/TabelaModel.php
+++ model/TabelaModel.php
@@ -1,16 +1,64 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of TabelaModel
- *
- * @author Admin
- */
 class TabelaModel {
-    //put your code here
+
+    protected $sql;
+    protected $table;
+    protected $colunaId;
+
+    public function todasColunas() {
+        $this->sql = "SELECT * FROM $this->table";
+    }
+
+    public function getSql() {
+        return $this->sql;
+    }
+
+    /*
+     * @var $colunas array
+     * Ex: ('id', 'nome', 'turma')
+     */
+
+    public function colunas(array $colunas) {
+        $stringColunas = '';
+        for ($i = 0; $i < sizeof($colunas); $i++) {
+            if ($i > 0) {
+                $stringColunas += ", ";
+            }
+            $stringColunas += $colunas[$i];
+        }
+        $this->sql = "SELECT $stringColunas FROM $this->table";
+    }
+
+    public function porId($id) {
+        $this->sql .= " WHERE $this->colunaId = $id";
+    }
+
+    /*
+     * @var $params array
+     * Ex: ('id' => '7', 'nome' => 'Pedro')
+     */
+
+    public function porParametros(array $params) {
+        $stringParams = ' WHERE ';
+        $i = 0;
+        foreach ($params as $param => $valor) {
+            if ($i > 0) {
+                $stringParams .= " AND ";
+            }
+            $stringParams .= "$param = $valor";
+            $i++;
+        }
+        $this->sql .= $stringParams;
+    }
+
+    public function executar() {
+        include ('config/conectar.php');
+        $sql = $this->sql;
+        $result = $conn->query($sql);
+        $conn->close();
+
+        return $result;
+    }
+
 }
